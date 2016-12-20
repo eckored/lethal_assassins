5531b9aae2216797fd3c24dc100fba7b43efaa48
eckored <ecko2056@gmail.com>
2016-12-20 00:45:45 -0600

    Incorporated bluesaberist's changes.

--- web/index.php
+++ web/index.php
@@ -22,7 +22,6 @@
 
 
 <!-- Top Logo -->
-<<<<<<< HEAD
 <table height="100" width="100%" borderwidth="0" padding="0" cellpadding="0" cellspacing="0">
   <tr>
     <td vAlign="center" align="center"><img src="http://i.imgur.com/Ae08bX6.png"/></td>
@@ -36,19 +35,6 @@
 <?php
 // Get DB Connection
   include_once "DBConnection.php";
-=======
-<table height="164" width="100%" borderwidth="0" padding="0" cellpadding="0" cellspacing="0">
-  <tr>
-<!--        <td style="BACKGROUND: url(img/main-top-bg2.png) no-repeat top " vAlign="center" align="center"><img src="img/LethalAssassins.png"></img><br><br><br><br></td> -->
-    <td vAlign="center" align="center"><img src="img/LethalAssassins.png"></img><br><br><br><br></td>
-  </tr>
-</table>
-
-<!-- Database Connection -->
-<?php
-// Get DB Connection
-  include_once "../DBConnection.php";
->>>>>>> origin/master
   try {
     $dbh = new PDO($db_connection_string, $db_user, $db_password);
   }
@@ -59,7 +45,6 @@
 
 
 <!-- Roster List -->
-<<<<<<< HEAD
 
 
 <center><table class="maintable" style="background: rgba(0,0,0,0.6)"><tr><td><div>- Roster -</div></center><br>
@@ -73,29 +58,11 @@
 
 <?php
 $memberQuery = "SELECT `id`, `rank`, `name`, `joindate`, `discordid` FROM Master ORDER BY `joindate`";
-=======
-<div>- Roster -</div>
-<center>
-<table class="rostertop" style="background: rgba(0,0,0,0.5)">
-  <tr>
-    <th class="rostera">Rank</th>
-    <th class="rosterb">Name</th>
-    <th class="rosterc">Join Date</th>
-    <th class="rosterd">Discord ID</th>
-  </tr>
-
-<?php
-$memberQuery = "SELECT `id`, `rank`, `name`, `joindate`, `discordid` FROM player WHERE rank = 1 ORDER BY `joindate`";
->>>>>>> origin/master
 $Master = $dbh->query($memberQuery);
 foreach ($Master as $user) {
   ?>
   <tr>
-<<<<<<< HEAD
     <td class="rostera"><?php echo $user["rank"] ?></td>
-=======
-    <td class="rostera">Clan Master</td>
->>>>>>> origin/master
     <td class="rosterb"><?php echo $user["name"] ?></td>
     <td class="rosterc"><?php echo $user["joindate"] ?></td>
     <td class="rosterd"><?php echo $user["discordid"] ?></td>
@@ -105,26 +72,15 @@ foreach ($Master as $user) {
 ?>
   </tr>
   <tr>
-<<<<<<< HEAD
     <th class="rosterth" colspan="4"></th>
   </tr>
 <?php
 $memberQuery = "SELECT `id`, `rank`, `name`, `joindate`, `discordid` FROM Officer ORDER BY `joindate`";
-=======
-    <th class="roster1" colspan="4"></th>
-  </tr>
-<?php
-$memberQuery = "SELECT `id`, `rank`, `name`, `joindate`, `discordid` FROM player WHERE rank = 2 OR rank = 3 ORDER BY rank ASC, `joindate`";
->>>>>>> origin/master
 $Officer = $dbh->query($memberQuery);
 foreach ($Officer as $user) {
   ?>
   <tr>
-<<<<<<< HEAD
     <td class="rostera"><?php echo $user["rank"] ?></td>
-=======
-    <td class="rostera">Officer</td>
->>>>>>> origin/master
     <td class="rosterb"><?php echo $user["name"] ?></td>
     <td class="rosterc"><?php echo $user["joindate"] ?></td>
     <td class="rosterd"><?php echo $user["discordid"] ?></td>
@@ -133,26 +89,15 @@ foreach ($Officer as $user) {
 }
 ?>
   <tr>
-<<<<<<< HEAD
     <th class="rosterth" colspan="4"></th>
   </tr>
 <?php
 $memberQuery = "SELECT `id`, `rank`, `name`, `joindate`, `discordid` FROM Fighter ORDER BY `joindate`";
-=======
-    <th class="roster1" colspan="4"></th>
-  </tr>
-<?php
-$memberQuery = "SELECT `id`, `rank`, `name`, `joindate`, `discordid` FROM player WHERE rank = 4 ORDER BY `joindate`";
->>>>>>> origin/master
 $Fighter = $dbh->query($memberQuery);
 foreach ($Fighter as $user) {
   ?>
   <tr>
-<<<<<<< HEAD
     <td class="rostera"><?php echo $user["rank"] ?></td>
-=======
-    <td class="rostera">Fighter</td>
->>>>>>> origin/master
     <td class="rosterb"><?php echo $user["name"] ?></td>
     <td class="rosterc"><?php echo $user["joindate"] ?></td>
     <td class="rosterd"><?php echo $user["discordid"] ?></td>
@@ -161,33 +106,21 @@ foreach ($Fighter as $user) {
 }
 ?>
   <tr>
-<<<<<<< HEAD
     <th class="rosterth" colspan="4"></th>
   </tr>
 <?php
 $memberQuery = "SELECT `id`, `rank`, `name`, `joindate`, `discordid` FROM Recruit ORDER BY `joindate`";
-=======
-    <th class="roster1" colspan="4"></th>
-  </tr>
-<?php
-$memberQuery = "SELECT `id`, `rank`, `name`, `joindate`, `discordid` FROM player WHERE rank = 13 ORDER BY `joindate`";
->>>>>>> origin/master
 $Recruit = $dbh->query($memberQuery);
 foreach ($Recruit as $user) {
   ?>
   <tr>
-<<<<<<< HEAD
     <td class="rostera"><?php echo $user["rank"] ?></td>
-=======
-    <td class="rostera">Recruit</td>
->>>>>>> origin/master
     <td class="rosterb"><?php echo $user["name"] ?></td>
     <td class="rosterc"><?php echo $user["joindate"] ?></td>
   </tr>
   <?php
 }
 ?>
-<<<<<<< HEAD
 </table></center>
 <br>
 <center><table class="rostercontact" style="background: rgba(0,0,0,0.4)">
@@ -225,46 +158,6 @@ foreach ($Recruit as $user) {
   <img class="mySlides" src="http://imgur.com/1qb5di1.jpg" style="width:100%"/>
   <img class="mySlides" src="http://imgur.com/hISbQb.jpgR" style="width:100%"/>
   <img class="mySlides" src="http://imgur.com/vOVN031.jpg" style="width:100%"/>
-=======
-</table>
-
-<table class="rostercontact" style="background: rgba(0,0,0,0.5)">
-<tr>
-<th class="roster"><div class="contact">Contact our Master or one of our Officer's if you are interested in joining forces with Lethal Assassins, or you wish to set up a Clan War.</div></th>
-</tr>
-</table>
-</center>
-
-<br><br><br>
-
-
-<!-- Screenshots -->
-<center><table id="maintable1" style="background: rgba(0,0,0,0.6)"><tr><td><div>- Screenshots -</div></td></tr></table></center><br>
-<center><table id="maintable2" style="background: rgba(0,0,0,0.6)"><tr><td><br>
-<div class="w3-content w3-display-container" style="max-width:700px">
-  <img class="mySlides" src="http://imgur.com/WuepXLd.jpg" style="width:100%">
-  <img class="mySlides" src="http://imgur.com/Mkz1oMj.jpg" style="width:100%">
-  <img class="mySlides" src="http://imgur.com/sMo48zc.jpg" style="width:100%">
-  <img class="mySlides" src="http://imgur.com/X1xyVRr.jpg" style="width:100%">
-  <img class="mySlides" src="http://imgur.com/QMo1A9R.jpg" style="width:100%">
-  <img class="mySlides" src="http://imgur.com/YLBbmBD.jpg" style="width:100%">
-  <img class="mySlides" src="http://imgur.com/3Iqrxkg.jpg" style="width:100%">
-  <img class="mySlides" src="http://imgur.com/43T8O96.jpg" style="width:100%">
-  <img class="mySlides" src="http://imgur.com/NvP0BVW.jpg" style="width:100%">
-  <img class="mySlides" src="http://imgur.com/JlVskZt.jpg" style="width:100%">
-  <img class="mySlides" src="http://imgur.com/IsZqwQW.jpg" style="width:100%">
-  <img class="mySlides" src="http://imgur.com/nOmAsvp.jpg" style="width:100%">
-  <img class="mySlides" src="http://imgur.com/e8cdv5g.jpg" style="width:100%">
-  <img class="mySlides" src="http://imgur.com/aEM8OKe.jpg" style="width:100%">
-  <img class="mySlides" src="http://imgur.com/ArxUbnx.jpg" style="width:100%">
-  <img class="mySlides" src="http://imgur.com/VBV9Ocg.jpg" style="width:100%">
-  <img class="mySlides" src="http://imgur.com/FPsxaxf.jpg" style="width:100%">
-  <img class="mySlides" src="http://imgur.com/Avm7zQE.jpg" style="width:100%">
-  <img class="mySlides" src="http://imgur.com/X5dzXw3.jpg" style="width:100%">
-  <img class="mySlides" src="http://imgur.com/1qb5di1.jpg" style="width:100%">
-  <img class="mySlides" src="http://imgur.com/hISbQb.jpgR" style="width:100%">
-  <img class="mySlides" src="http://imgur.com/vOVN031.jpg" style="width:100%">
->>>>>>> origin/master
   <div class="w3-center w3-section w3-large w3-text-white w3-display-bottommiddle" style="width:100%">
     <div class="w3-left w3-padding-left w3-hover-text-khaki" onclick="plusDivs(-1)">&#10094;</div>
     <div class="w3-right w3-padding-right w3-hover-text-khaki" onclick="plusDivs(1)">&#10095;</div>
@@ -295,7 +188,6 @@ foreach ($Recruit as $user) {
 <script>var slideIndex = 1;showDivs(slideIndex);function plusDivs(n) {showDivs(slideIndex += n);}function currentDiv(n) {showDivs(slideIndex = n);}function showDivs(n) {var i;var x = document.getElementsByClassName("mySlides");var dots = document.getElementsByClassName("demo");if (n > x.length) {slideIndex = 1}if (n < 1) {slideIndex = x.length}for (i = 0; i < x.length; i++) {x[i].style.display = "none";}for (i = 0; i < dots.length; i++) {dots[i].className = dots[i].className.replace(" w3-white", "");}x[slideIndex-1].style.display = "block";dots[slideIndex-1].className += " w3-white";}</script>
 <br>
 </td></tr></table></center>
-<<<<<<< HEAD
 <br><br>
 
 
@@ -318,37 +210,3 @@ foreach ($Recruit as $user) {
 </td></tr></table></center>
 </noscroll></body>
 </html>
-=======
-<br><br><br>
-
-<script>
-var slideIndex = 1;
-showDivs(slideIndex);
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-     dots[i].className = dots[i].className.replace(" w3-white", "");
-  }
-  x[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " w3-white";
-}
-</script>
-
-
-
-</noscroll></body>
-</html>
->>>>>>> origin/master
